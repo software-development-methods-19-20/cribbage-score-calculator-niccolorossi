bfe5a681bcb0fbe1f204d683f26b53254927c660
Niccolò <rossinikolai@gmail.com>
2019-11-27 17:48:06 +0100

    fixed double runs

--- production/source/dssc/cribbage/RunsScore.java
+++ production/source/dssc/cribbage/RunsScore.java
@@ -1,5 +1,6 @@
 package dssc.cribbage;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,20 +13,17 @@ public class RunsScore {
         Collections.sort(values, comparator);
 
        List<Integer> sortedAndParsed = values.stream().map(x -> comparator.getRunPlace(x)).collect(Collectors.toList());
+       List<Integer> uniqueValuesList = sortedAndParsed.stream().distinct().collect(Collectors.toList());
+
+       int multiplier = (sortedAndParsed.size()-uniqueValuesList.size()) + 1;
 
        int count = 0;
-       for (int i = 0; i < sortedAndParsed.size() - 1; i++) {
-
-           int j = i + 1;
-
-           while (sortedAndParsed.get(j) == sortedAndParsed.get(i)) {
-               j++;
-           }
-
-           if (sortedAndParsed.get(i) +1  != sortedAndParsed.get(j)) {
+       for (int i = 0; i < uniqueValuesList.size() - 1; i++) {
+           if (uniqueValuesList.get(i) +1  != uniqueValuesList.get(i+1)) {
                continue;
            } else count++;
        }
-       return count+1;
+       if (count != 0) count += 1;
+       return count*multiplier;
     }
 }
--- test/source/test/CribbageScoreTest.java
+++ test/source/test/CribbageScoreTest.java
@@ -3,6 +3,8 @@ package test;
 import dssc.cribbage.CribbageHand;
 import org.junit.jupiter.api.Test;
 
+import javax.security.auth.login.CredentialException;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -24,6 +26,12 @@ public class CribbageScoreTest {
     void testFourRun() {
         CribbageHand cribbageHand = new CribbageHand("6DJH4H7C5H");
         assertThat(cribbageHand.runsScore(), is(4));
+    }
+
+    @Test
+    void testEightRun() {
+        CribbageHand cribbageHand = new CribbageHand("5S4S2S6H5H");
+        assertThat(cribbageHand.runsScore(), is(6));
     }
 
 }
